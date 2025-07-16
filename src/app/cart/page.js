5be334c3259f2ebe5283d61f67c3f2ed858569df
kaustubha-saxena <kaustubhasaxena@gmail.com>
2025-07-17 01:52:52 +0530

    updated

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -57,11 +57,11 @@ shipping=40
               colors="primary:#2a2a2a,secondary:#2a2a2a"
               width={300}
               height={300}
-              className="xl:h-30 xl:w-30 md:h-15 md:w-15 sm:w-20 sm:h-20"
+              className="xl:h-30 xl:w-30 md:h-15 md:w-15 sm:w-20 sm:h-20 w-15 h-15"
             >
             </lord-icon></div>
-            <p className='md:text-lg lg:text-xl xl:text-2xl text-xl sm:font-bold'>Your cart is empty!</p>
-            <p className=' md:text-xs lg:text-base xl:text-lg font-medium'>It&apos;s a good day to buy the items you saved for later</p>
+            <p className='md:text-lg lg:text-xl xl:text-2xl sm:text-xl text-xl font-bold '>Your cart is empty!</p>
+            <p className=' md:text-xs lg:text-base xl:text-lg text-sm font-medium'>It&apos;s a good day to buy the items you saved for later</p>
             <div className=' w-full flex justify-center items-center my-5'>
 
             <Link href={"/"}>
@@ -72,25 +72,25 @@ shipping=40
     }
           
         </div>
-        <div className='w-[50%] md:px-10 h-fit pt-20 md:ml-5 mx-auto   '>
+        <div className='md:w-[50%] md:px-10 h-fit pt-20 md:ml-5 mx-auto   '>
           <div>
-            <span className='md:text-lg xl:text-xl sm:text-xl font-bold'>Price Summary</span>
+            <span className='md:text-lg xl:text-xl sm:text-xl text-lg font-bold'>Price Summary</span>
           </div>
           <div className='px-3'>
 
-            <div className='flex justify-between items-center md:text-sm lg:text-base xl:text-lg sm:text-lg font-medium text-[#bcbcbc]'>
+            <div className='flex justify-between items-center md:text-sm lg:text-base xl:text-lg sm:text-lg text-base font-medium text-[#bcbcbc]'>
               <p>Order Total</p>
               <p> ₹ {totalMRP}.00</p>
             </div>
-            <div className='flex justify-between items-center md:text-sm lg:text-base xl:text-lg sm:text-lg font-medium text-[#bcbcbc]'>
+            <div className='flex justify-between items-center md:text-sm lg:text-base xl:text-lg sm:text-lg text-base font-medium text-[#bcbcbc]'>
               <p>Items Discount</p>
               <p>₹ {totalMRP-totalSalePrice}.00</p>
             </div>
-            <div className='flex justify-between items-center md:text-sm lg:text-base xl:text-lg sm:text-lg font-medium text-[#bcbcbc]'>
+            <div className='flex justify-between items-center md:text-sm lg:text-base xl:text-lg sm:text-lg text-base font-medium text-[#bcbcbc]'>
               <p>Shipping</p>
               <p>₹ {shipping}.00</p>
             </div>
-            <div className='flex justify-between items-center md:text-base lg:text-lg  xl:text-xl sm:text-xl font-bold my-4 '>
+            <div className='flex justify-between items-center md:text-base lg:text-lg  xl:text-xl sm:text-xl text-xl font-bold my-4 '>
               <p>To Pay</p>
               <p>₹ {totalSalePrice+shipping}.00</p>
             </div>
@@ -98,7 +98,7 @@ shipping=40
 
           <div className=' w-full flex justify-center items-center md:mt-10 mt-18'>
 
-            <button className=' mx-auto   md:px-15 lg:px-20 px-30 py-3 bg-amber-400 md:text-base  lg:text-lg xl:text-xl hover:cursor-pointer font-bold'>Buy Now</button>
+            <button className=' mx-auto text-[#2a2a2a]   md:px-15 lg:px-20 px-30 py-3 bg-amber-400 md:text-base  lg:text-lg xl:text-xl hover:cursor-pointer font-bold'>Buy Now</button>
           </div>
 
 
